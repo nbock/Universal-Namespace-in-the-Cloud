ddf3d1ba20a6028203a2baff3dc2d1242bc02b45
nbock <bocknolan@gmail.com>
2021-04-07 12:24:03 -0400

    beginning of automation script

---
+++ automation/install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+function parse_yaml {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+if ! command -v go $> /dev/null
+then
+  echo "Go not found, install go before continuing"
+  exit
+elif ! command -v upspin $> /dev/null
+then
+  echo "Upspin not found, install upspin before continuing"
+  exit
+elif ! command -v oc $> /dev/null
+then
+  echo "Openshift command line tool not found, install before continuing"
+  exit
+elif ! command -v docker $> /dev/null
+then
+  echo "Docker not found, install or start docker daemon before continuing"
+  exit
+fi
+
+# store all the yaml params as variables
+eval $(parse_yaml params.yaml)
+
+docker login -u $upspin_username -p $upspin_password
