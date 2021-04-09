de6124eecfd9a03eb1964a5417fe06ddd48c7da5
Jiapei1001 <jiapeili@live.com>
2021-04-09 10:27:34 -0700

    Update persistent volume settings

--- buildnDeploy/start.sh
+++ buildnDeploy/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < /home/upspin/passwd.template > /tmp/passwd
+envsubst < ${HOME}/passwd.template > /tmp/passwd
 export LD_PRELOAD=/usr/lib/libnss_wrapper.so
 export NSS_WRAPPER_PASSWD=/tmp/passwd
 export NSS_WRAPPER_GROUP=/etc/group
